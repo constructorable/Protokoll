e9cfb08053f08db3592d174f1a896ee3f84c5934
constructorable <acker_oliver@yahoo.de>
2025-04-19 01:33:36 +0200

    Update printjs.js

--- printjs.js
+++ printjs.js
@@ -103,7 +103,7 @@ document.getElementById('savePdfButton').addEventListener('click', async functio
                     letterRendering: true
                 });
 
-                const imgData = canvas.toDataURL('image/jpeg', 0.4);
+                const imgData = canvas.toDataURL('image/jpeg', 0.5);
                 const imgWidth = canvas.width;
                 const imgHeight = canvas.height;
 
