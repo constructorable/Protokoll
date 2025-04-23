6e8482b516104d3b93e943384362f2af50344897
constructorable <acker_oliver@yahoo.de>
2025-04-23 21:52:59 +0200

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,3 +1,7 @@
+/* Copyright - Oliver Acker, acker_oliver@yahoo.de
+connection.js
+Version 3.32_beta */
+
 document.addEventListener('DOMContentLoaded', function () {
     const connectionIcon = document.getElementById('connection-status');
 
