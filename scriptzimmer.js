f3641b01db769d4d809fbdb55a8fc589ca8e0578
constructorable <acker_oliver@yahoo.de>
2025-04-23 21:55:58 +0200

    Update scriptzimmer.js

--- scriptzimmer.js
+++ scriptzimmer.js
@@ -1,6 +1,6 @@
-// Copyright - Oliver Acker, acker_oliver@yahoo.de
-// scriptzimmer.js
-// Version 3.30_beta
+/* Copyright - Oliver Acker, acker_oliver@yahoo.de
+scriptzimmer.js
+Version 3.32_beta */
 
 function initializeSuggestionInputs() {
     document.querySelectorAll(".suggestion-input").forEach(inputField => {
