2add012acbb8c9152f236b4a4b521a500cbdb82a
constructorable <acker_oliver@yahoo.de>
2025-04-27 21:33:02 +0200

    Update printjs.js

--- printjs.js
+++ printjs.js
@@ -1,6 +1,6 @@
 /* Copyright - Oliver Acker, acker_oliver@yahoo.de
-printjs.js
-Version 3.32_beta */
+connection.js
+Version 3.34_beta */
 
 document.addEventListener('DOMContentLoaded', function () {
 });
